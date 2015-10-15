a6609b93b195996c535f7d8907f190df714dd065
Satyajeet Parida <shatyajeet@P1-SATYAJEET.local>
2015-10-15 17:22:35 +0530

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+var goToEl = function (ele) {
+  var scrollTop = $(ele).position().top - 50;
+  $('html, body').animate({
+    scrollTop: scrollTop + 'px'
+  }, 1000);
+};
+
+$(document).ready(function () {
+  setTimeout(function () {
+    $('.parallax').parallax();
+  }, 100);
+
+  $('body').on('click', '.hover-heading', function (e) {
+    var text = $(e.currentTarget).siblings('.hover-text');
+    if (text.hasClass('in')) {
+      text.slideDown(500, function () {
+        text.removeClass('in');
+        text.addClass('out');
+        goToEl(text);
+      });
+    } else if (text.hasClass('out')) {
+      text.slideUp(500, function () {
+        text.removeClass('out');
+        text.addClass('in');
+      });
+    }
+  });
+
+  $(".button-collapse").sideNav();
+
+  $('.collapsible').collapsible({
+    accordion : false
+  });
+});
---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About us' });
+});
+
+router.get('/story', function (req, res, next) {
+  res.render('story', { title: 'Our story so far' });
+});
+
+router.get('/unique', function (req, res, next) {
+  res.render('unique', { title: 'What makes us unique' });
+});
+
+router.get('/model', function (req, res, next) {
+  res.render('model', { title: 'Our operating model' });
+});
+
+router.get('/team', function (req, res, next) {
+  res.render('team', { title: 'Our team' });
+});
+
+router.get('/work', function (req, res, next) {
+  res.render('work', { title: 'Our work' });
+});
+
+router.get('/join', function (req, res, next) {
+  res.render('join', { title: 'Join us' });
+});
+
+router.get('/donate', function (req, res, next) {
+  res.render('donate', { title: 'Donate' });
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact', { title: 'Contact us' });
+});
+
+module.exports = router;
